81accf2abbbe7ced3e4104a4b1edc520ea17eeca
Onaefe <onaefe@onaxsys.com>
2022-04-26 21:00:42 +0100

    Progress bar component addition

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,8 @@ import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget, { WidgetOptions } from "../../components/widgets/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 const Home = () => {
   return (
     <div className="home">
@@ -13,6 +15,10 @@ const Home = () => {
           <Widget type={WidgetOptions.EARNING}/>
           <Widget type={WidgetOptions.BALANCE}/>
           <Widget type={WidgetOptions.ORDER}/>
+        </div>
+        <div className="charts">
+          <Featured/>
+          <Chart/>
         </div>
       </div>
     </div>
